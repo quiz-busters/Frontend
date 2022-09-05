e3e1f3105d3c64760472a5f847993b0bb6c9ab4c
B9ary01 <bishalrai111@yahoo.com>
2022-09-05 11:06:42 +0100

    add quizform page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { QuizForm } from "./pages";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
@@ -9,6 +10,8 @@ function App() {
     <Routes> 
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/quiz" element={<QuizForm/>}/>
+
       <Route path="/*" element={<h1>Page not found!</h1>}/>
     </Routes>
     </>
---
+++ src/pages/QuizForm/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const QuizForm=()=>{
+   
+    return(
+        <>
+           <h1>QuizForm page</h1>
+        </>
+
+    )
+
+}
+
+export default QuizForm;
