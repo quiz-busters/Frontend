8e9df3060369120db2c71d6f23a3c582d00efcf2
B9ary01 <bishalrai111@yahoo.com>
2022-09-05 13:26:53 +0100

    add quiz form

--- src/pages/QuizForm/index.jsx
+++ src/pages/QuizForm/index.jsx
@@ -1,14 +1,67 @@
 import React from "react";
+import { Button, MenuItem, TextField } from "@material-ui/core";
+import "./quizform.css";
 
 const QuizForm=()=>{
-   
-    return(
-        <>
-           <h1>QuizForm page</h1>
-        </>
 
+    return(<>
+      
+      <div className="content">
+
+        <div className="form_settings">
+          <span style={{ fontSize: 30 }}>QUIZ TIME</span>
+
+          <div className="select_settings">
+            <TextField
+              style={{ marginBottom: 25 }}
+              label="Enter Your username"
+              variant="outlined"/>
+
+           
+         
+
+              <TextField
+              select
+              label="Select Category"
+              variant="outlined"
+              style={{ marginBottom: 30 }}
+           
+             >
+            
+            </TextField>
+
+            <TextField
+              select
+              label="Select Difficulty"
+              variant="outlined"
+              style={{ marginBottom: 30 }}
+             
+            >
+              <MenuItem key="Easy" value="easy">
+                Easy
+              </MenuItem>
+              <MenuItem key="Medium" value="medium">
+                Medium
+              </MenuItem>
+              <MenuItem key="Hard" value="hard">
+                Hard
+              </MenuItem>
+            </TextField>
+
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+            
+            >
+              Start Quiz
+            </Button>
+          </div>
+        </div>
+        
+      </div>
+
+      </>
     )
-
 }
-
-export default QuizForm;
+export default QuizForm 
