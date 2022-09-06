d743f319c61e418beb67cb0dcb5ea4fafe5d9b67
B9ary01 <bishalrai111@yahoo.com>
2022-09-06 14:50:13 +0100

    change url

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import axios from 'axios';
 
 import './index.css';
 import App from './App';
-// axios.defaults.baseURL = "https://quiz-busters.herokuapp.com";
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://quiz-busters.herokuapp.com";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
